f191f264e5297b3721f7d8d7a1fd935f328864ba
shdalton <stephane.h.dalton@gmail.com>
2023-11-13 18:22:24 -0500

    adding circle implementation

--- app/src/main/java/atelier11/Circle.java
+++ app/src/main/java/atelier11/Circle.java
@@ -1,5 +1,44 @@
 package atelier11;
 
-public class Circle {
+public class Circle extends Shape{
+
+    private double radius = 1.0;
+
+    public Circle() { radius = 1.0;}
+
+    public Circle(double radius) { this. radius = radius;}
+
+    public Circle(double radius, String color, boolean filled) 
+    { 
+        super(color, filled);
+        this. radius = radius;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double getArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter()
+    {
+        return Math.PI * 2 * radius;
+    }
+
+    public String toString()
+    {
+        return "Circle " + super.toString() + "radius :" + radius;
+    }
+
+
     
 }
